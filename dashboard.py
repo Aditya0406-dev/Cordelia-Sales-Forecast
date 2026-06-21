80435bc580ecaa3b4f8ac91785695c1ca571c453
Aditya0406-dev <adityasubramanian2006@gmail.com>
2026-06-21 22:18:56 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -27,7 +27,6 @@ def load_css(file_name):
     with open(file_name, "r") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
 # Run the function right away
 load_css(os.path.join(CURRENT_DIR, "style.css"))
 
